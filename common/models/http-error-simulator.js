43890c1b0a2655f4aceeca1d5959d0812fa50c95
Benjamin Wong <benjamin.wss@gmail.com>
2019-05-23 21:23:29 +0800

    Added baseline error endpoint and added HTTP Get error generator.

---
+++ common/models/http-error-simulator.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = HttpErrorSimulator => {
+  function generalErrorGenerator({ errorCode, callback }) {
+    const errorMessage = `Simulated ${errorCode} error`;
+
+    const error = new Error(errorMessage);
+    error.status = errorCode;
+
+    callback(error, null);
+  }
+
+  HttpErrorSimulator.httpGetError = (errorCode, callback) => {
+    generalErrorGenerator({ errorCode, callback });
+  };
+};
